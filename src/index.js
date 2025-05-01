a5d762ee624f553e81d22e396ca6716ef5e08c1b
Mandira <mandeerashiwakoti@gmail.com>
2025-05-01 23:30:51 +0545

    all setup configure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
+import express from 'express';
 
 import connectDB from "./db/index.js";
+const app=express();
 
 dotenv.config({
   path: "./env",
@@ -20,6 +22,7 @@ connectDB()
   .catch((err) => {
     console.log("MONGODB connection failed !!!", err);
   });
+  export default app
 
 // import express from 'express'
 
